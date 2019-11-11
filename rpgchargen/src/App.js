6aef769c4a30f05e669eda59c33f718562ede11d
Quibbly <owenhedwards@gmail.com>
2019-11-11 23:23:17 +0000

    Front end mostly finished

--- rpgchargen/src/App.js
+++ rpgchargen/src/App.js
@@ -7,9 +7,9 @@ import SubmitButtons from './components/submitButtons/SubmitButtons';
 const initialState = {
   submitted: false,
   fullRandom: true,
-  role:'',
-  race: '',
-  system: '',
+  role:'Random',
+  race: 'Random',
+  system: 'Random',
 }
 
 class App extends Component {
@@ -18,10 +18,25 @@ class App extends Component {
     this.state = initialState;
   }
 
+  //HANDLES SUBMIT BUTTON
   submit = () => {
     this.setState({
       submitted: true
     });
+    console.log(this.state.role);
+    console.log(this.state.race);
+    console.log(this.state.system);
+  }
+
+  //HANDLES DROP OPTIONS CHANGES
+  handleRoleChange = (event) => {
+    this.setState({role: event.target.value});
+  }
+  handleRaceChange = (event) => {
+    this.setState({race: event.target.value});
+  }
+  handleSystemChange = (event) => {
+    this.setState({system: event.target.value});
   }
 
 
@@ -32,7 +47,7 @@ class App extends Component {
         <h1>The RPG character generator</h1>
         <h2>Who are you looking for?</h2>
 
-        <SelectionButtons role={this.state.role} race={this.state.race} system={this.state.system}/>
+        <SelectionButtons handleRaceChange={this.handleRaceChange} handleSystemChange={this.handleSystemChange} handleRoleChange={this.handleRoleChange}/>
         <SubmitButtons submit={this.submit} fullRandom={this.state.fullRandom} submitted={this.state.submitted}/>
         { submitted === true
           ? <InfoCards role={this.state.role} race={this.state.race} system={this.state.system}/>
--- rpgchargen/src/components/selectionButtons/SelectionButtons.js
+++ rpgchargen/src/components/selectionButtons/SelectionButtons.js
@@ -1,30 +1,34 @@
 import React from 'react';
 
 
-const SelectionButtons = ({role, race, system}) => {
+const SelectionButtons = ({role, handleRoleChange, handleRaceChange,handleSystemChange,fullRandom, submit, submitted}) => {
    return (
-      <div>
-         <select id="classSelection">
-            <option value = "1">One</option>
-            <option value = "2">Two</option>
-            <option value = "3">Three</option>
-            <option value = "4">Four</option>
-         </select>
+      <form>
+         <div>
+            <select onChange={handleRoleChange}>
+               <option value = "Random">Random</option>
+               <option value = "Warrior">Warrior</option>
+               <option value = "Hunter">Hunter</option>
+               <option value = "Monk">Monk</option>
+            </select>
 
-         <select id="raceSelection">
-            <option value = "1">One</option>
-            <option value = "2">Two</option>
-            <option value = "3">Three</option>
-            <option value = "4">Four</option>
-         </select>
 
-         <select id="systemSelection">
-            <option value = "1">One</option>
-            <option value = "2">Two</option>
-            <option value = "3">Three</option>
-            <option value = "4">Four</option>
-         </select>
-      </div>
+
+            <select onChange={handleRaceChange}>
+               <option value = "Random">Random</option>
+               <option value = "Human">Human</option>
+               <option value = "Orc">Orc</option>
+               <option value = "Elf">Elf</option>
+            </select>
+
+
+
+            <select onChange={handleSystemChange}>
+               <option value = "Random">Random</option>
+               <option value = "Dnd 5e">DnD 5e</option>
+            </select>
+         </div>
+      </form>
    );
 }
 
