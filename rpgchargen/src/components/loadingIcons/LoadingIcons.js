f9cd9fda974cb1af0f0911988c1518006bed39b3
Quibbly <owenhedwards@gmail.com>
2019-11-21 13:47:19 +0000

    Cleaned up old folders

--- rpgchargen/src/components/loadingIcons/LoadingIcons.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactLoading from 'react-loading';
- 
-const LoadingIcons = ({ type, color }) => (
-   <ReactLoading type={'balls'} color="#fcba03" />
-);
- 
-export default LoadingIcons;
